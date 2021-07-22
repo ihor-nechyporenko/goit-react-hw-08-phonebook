446e195905c946015f885ef597f0f791c8495df4
Ihor Nechyporenko <nechyporenko.i@gmail.com>
2021-07-22 13:07:58 +0300

    Make code splitting

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import { Component } from 'react';
+import { Component, Suspense, lazy } from 'react';
 import { connect } from 'react-redux';
 import { Switch } from 'react-router-dom';
 import { CSSTransition } from 'react-transition-group';
 
 import Container from './components/Container';
 import Header from './components/Header';
-// import Form from './components/Form';
-// import Filter from './components/Filter';
-// import ContactList from './components/ContactList';
-// import Loader from './components/Loader';
-// import Error from './components/Error';
-// import { operations, selectors } from './redux/phonebook';
-import { authOperations } from './redux/auth';
-
-import HomePage from './pages/HomePage';
-import RegisterPage from './pages/RegisterPage';
-import LoginPage from './pages/LoginPage';
-import ContactsPage from './pages/ContactsPage';
+import Loader from './components/Loader';
 import PrivateRoute from './components/UserMenu/PrivateRoute';
 import PublicRoute from './components/UserMenu/PublicRoute';
+import { authOperations } from './redux/auth';
 
 import './common.css';
-// import fadeStyles from './fade/fadeFilter.module.css';
 import fadeHeaderStyles from './fade/fadeHeader.module.css';
+
+const HomePage = lazy(() =>
+  import('./pages/HomePage' /* webpackChunkName: "home-page" */),
+);
+const RegisterPage = lazy(() =>
+  import('./pages/RegisterPage' /* webpackChunkName: "register-page" */),
+);
+const LoginPage = lazy(() =>
+  import('./pages/LoginPage' /* webpackChunkName: "login-page" */),
+);
+const ContactsPage = lazy(() =>
+  import('./pages/ContactsPage' /* webpackChunkName: "contacts-page" */),
+);
 
 class App extends Component {
   componentDidMount() {
@@ -32,27 +34,6 @@ class App extends Component {
   render() {
     return (
       <Container>
-        <Switch>
-          <PublicRoute exact path="/" component={HomePage} />
-          <PublicRoute
-            path="/register"
-            restricted
-            redirectTo="/contacts"
-            component={RegisterPage}
-          />
-          <PublicRoute
-            path="/login"
-            restricted
-            redirectTo="/contacts"
-            component={LoginPage}
-          />
-          <PrivateRoute
-            path="/contacts"
-            redirectTo="/login"
-            component={ContactsPage}
-          />
-        </Switch>
-
         <CSSTransition
           in={true}
           appear
@@ -62,6 +43,29 @@ class App extends Component {
         >
           <Header />
         </CSSTransition>
+
+        <Suspense fallback={<Loader />}>
+          <Switch>
+            <PublicRoute exact path="/" component={HomePage} />
+            <PublicRoute
+              path="/register"
+              restricted
+              redirectTo="/contacts"
+              component={RegisterPage}
+            />
+            <PublicRoute
+              path="/login"
+              restricted
+              redirectTo="/contacts"
+              component={LoginPage}
+            />
+            <PrivateRoute
+              path="/contacts"
+              redirectTo="/login"
+              component={ContactsPage}
+            />
+          </Switch>
+        </Suspense>
       </Container>
     );
   }
